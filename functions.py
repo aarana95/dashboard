05155d8a305b84f086d488f51c6bed32a7ffc145
anderTheBridge <ander@thebridge.tech>
2021-03-13 16:55:48 +0100

    clean code

--- functions.py
+++ functions.py
@@ -10,13 +10,14 @@ from PIL import Image
 def configuracion():
     st.set_page_config(page_title='Dashboard The Bridge', page_icon='https://www.google.com/s2/favicons?domain=www.thebridge.tech', layout="wide")
 
+
+# Carga de datos
 @st.cache
 def cargar_creadenciales(key_file):
     key_file.seek(0)
     key_json = json.load(key_file)
 
     return key_json
-
 def connect_to_sheet(key_json):
 
 
@@ -30,118 +31,204 @@ def connect_to_sheet(key_json):
     client = gspread.authorize(creds)
 
     return(client)
-
-#@st.cache(hash_funcs={"gspread.client.Client": id})
 def open_sheet(client, sheet_name, worksheet=0):
     sheet = client.open(sheet_name)
     worksheet = sheet.get_worksheet(worksheet)
 
     return worksheet
-
-#@st.cache(hash_funcs={"gspread.models.Worksheet": id})
 def get_df(worksheet):
 
-    dataframe = pd.DataFrame(worksheet.get_all_records())
-    return(dataframe)
+    df = pd.DataFrame(worksheet.get_all_records())
 
-def home():
-    img = Image.open('logo.png')
+    df = df[-df['EMAIL'].isin([''])]
+    df.rename(columns={'': 'Nombres'}, inplace=True)
+
+    return(df)
+
+
+# Menus
+def menu_general(df, imagen):
+    home(imagen)
+    kpi_escuela(df)
+    pie_tipos(df)
+def menu_contratacion(df):
+
+    st.title("Estados de Contratación")
+    kpi_busqueda_empleo(df)
+
+    with st.beta_expander('Opciones:'):
+        normalizar = st.checkbox('Normalizar todos los bootcamps.')
+        barras = st.checkbox('Barras separadas')
+
+
+    estado_alumnos(df, 'Estado actual', normalizar, barras)
+    estado_alumnos(df, 'Estado 90 días post-graduación', normalizar, barras)
+    estado_alumnos(df, 'Estado 180 días post-graduación', normalizar, barras)
+def menu_trabajando(df):
+
+    st.title("Alumnos Empleados")
+
+    working = df[df['Fecha Contrataciön'] != '']
+
+    #DEBERIAMOS DE ELIMINAR ESTO CUANDO EL EXCEL ESTE BIEN
+    #Inconsistencias en el excel
+    working = working[working['Fecha Contrataciön'].apply(lambda x: len(x)) == 10]
+    #MdC fue contratada un dia que no existe
+    working = working.drop(67)
+
+
+    fecha_contratacion = pd.to_datetime(working['Fecha Contrataciön'], format='%d/%m/%Y')
+    fecha_inicio = pd.to_datetime(working['Promoción'], format='%y.%m')
+    working['Dias hasta que encuentra trabajo'] = (fecha_contratacion - fecha_inicio).dt.days
+
+
+    col1, col2 = st.beta_columns([1, 1])
+
+    #Tiempo de espera vs bootcamp
+    with col1:
+        fig = px.violin(working, y="Dias hasta que encuentra trabajo", color="Programa", box=True)
+
+        st.header("Tiempo de espera por programa.")
+        st.plotly_chart(fig, use_container_width=True)
+
+    #Tiempo de espera vs promocion
+    with col2:
+        fig = px.violin(working, y="Dias hasta que encuentra trabajo", color="Promoción", box=True)
+
+        st.header("Tiempo de espera por promoción")
+        st.plotly_chart(fig, use_container_width=True)
+
+
+    col1, col2 = st.beta_columns([1, 1])
+
+    with col1:
+
+        with_salary = working[working['Salario Bruto Anual Primera Contratación'] != '']
+        with_salary['Salario Bruto Anual Primera Contratación'] = \
+            with_salary['Salario Bruto Anual Primera Contratación'].apply(lambda x: x*1000 if x < 100 else x)
+
+        fig = px.scatter(with_salary, x="Dias hasta que encuentra trabajo",
+                         y="Salario Bruto Anual Primera Contratación", hover_data=['Nombres'])
+
+        st.header("Salario vs Días para encontrar trabajo")
+        st.plotly_chart(fig, use_container_width=True)
+
+    with col2:
+
+        empresas = working['Empresa de primera contratación'].value_counts()[
+            working['Empresa de primera contratación'].value_counts() > 1]
+
+        empresas = empresas[empresas.index != ''].sort_values()
+        fig = px.bar(y=empresas.index, x=empresas, orientation='h')
+
+        st.header("Empresas que más contratan:")
+        st.plotly_chart(fig, use_container_width=True)
+
+
+# Partes menu general
+def home(imagen):
+    img = Image.open(imagen)
     st.image(img)
-
 def kpi_escuela(df):
     streamlit_metrics.metric_row({
         'Nº Alumnos': df.shape[0],
         'Nº Promociones': len(df['Promoción'].unique()),
         '% Alumnos Graduados': round(df['Estado de matrícula'].value_counts(normalize=True)['Graduado'] * 100, 0),
     })
-
 def pie_tipos(df):
+
     col1, col2, col3 = st.beta_columns([1, 1, 1])
 
     with col1:
-        st.header("Programa")
-        program_values = df['Programa'].value_counts()
 
-        fig = px.pie(names=program_values.index, values=program_values.values)
-        fig.update_traces(hoverinfo='label+percent+name')
-        # fig.update(layout_showlegend=False)
-        st.plotly_chart(fig, use_container_width=True)
+        crear_pie(df, "Programa")
 
     with col2:
-        st.header("Promocion")
-        program_values = df['Promoción'].value_counts()
-        fig = px.pie(names=program_values.index, values=program_values.values)
-        fig.update_traces(hoverinfo='label+percent+name')
-        # fig.update(layout_showlegend=False)
-        st.plotly_chart(fig, use_container_width=True)
 
-#    with col3:
-#        st.header("Horario")
-#        program_values = df['Horario'].value_counts()
-#        fig = px.pie(names=program_values.index, values=program_values.values)
-#        fig.update_traces(hoverinfo='label+percent+name')
-#        st.plotly_chart(fig, use_container_width=True)
+        crear_pie(df, "Promoción")
 
     with col3:
-        st.header("Motivación")
-        program_values = df['Motivación'].value_counts()
-        fig = px.pie(names=program_values.index, values=program_values.values)
-        fig.update_traces(hoverinfo='label+percent+name')
-        fig.update(layout_showlegend=False)
+
+        crear_pie(df, "Motivación", False)
+def crear_pie(df, variable, layout=True):
+
+    st.header(variable)
+
+    var_values = df[variable].value_counts()
+
+    fig = px.pie(names=var_values.index, values=var_values.values)
+    fig.update_traces(hoverinfo='label+percent+name')
+    fig.update(layout_showlegend=layout)
+
+    st.plotly_chart(fig, use_container_width=True)
+
+
+# Partes menu contratación
+def kpi_busqueda_empleo(df):
+
+    tipo = '3 Continua su búsqueda de trabajo'
+
+    a_180 = kpi_empleo(df, 'Estado 180 días post-graduación', tipo)
+    a_90 = kpi_empleo(df, 'Estado 90 días post-graduación', tipo)
+    actual = kpi_empleo(df, 'Estado actual', tipo)
+
+    streamlit_metrics.metric_row({
+        '% Alumnos que Continuan buscando trabajo':actual,
+        '% Alumnos que Continuan buscando trabajo a 90 días de graduarse':a_90,
+        '% Alumnos que Continuan buscando trabajo a 180 días de graduarse':a_180
+    })
+def kpi_empleo(df, momento, type):
+
+    try:
+        metr = df.loc[-df[momento].isin(['']), momento].value_counts(normalize=True)[type] * 100
+        metr = round(metr, 0)
+    except:
+        metr = 0
+
+    return metr
+def estado_alumnos(df, momento, normalizar, barras):
+
+    barmode = 'relative'
+    if barras:
+        barmode = 'group'
+
+    progrmas = df.groupby(['Programa', momento])['Nombres'].count()
+    progrmas = progrmas.reset_index()
+    progrmas = progrmas[(progrmas.iloc[:, 1] != '') & (progrmas.iloc[:, 0] != '')]
+    progrmas.sort_values(momento, inplace=True)
+
+    if normalizar:
+
+        progrmas['EMAIL'] /= progrmas.groupby("Programa")["EMAIL"].transform(sum)
+
+
+    with st.beta_expander(momento, expanded=True):
+
+        fig = px.bar(progrmas, x="Programa", y="Nombres", color=momento,
+                      color_discrete_map={'1A Empleado a jornada completa': 'rgb(46, 204, 113)',
+                                                        '1B Prácticas a jornada completa': "rgb(46, 204, 113)",
+                                                        '1C Contrato temporal o a jornada parcial': "rgb(46, 204, 113)",
+                                                        '1D Emprende proyecto propio': 'rgb(46, 204, 113)',
+                                                        '2A Empleado en trabajo no relacionado con el bootcamp': 'rgb(241, 196, 15)',
+                                                        '2B Continuando con otros estudios': 'rgb(241, 196, 15)',
+                                                        '2C No busca trabajo por razones de salud, familiares o personales': 'rgb(241, 196, 15)',
+                                                        '3 Continua su búsqueda de trabajo': 'rgb(176, 58, 46)',
+                                                        '4 Sin información': 'rgb(33, 47, 60)',
+                                                        'Excluido de CIRR': 'rgb(33, 47, 60)'},
+                     labels=None, barmode=barmode, hover_data=['Nombres'])
+
+
         st.plotly_chart(fig, use_container_width=True)
 
-def kpi_busqueda_empleo(df):
-    try:
-        a_180 = round(df.loc[-df['Estado 180 días post-graduación'].isin(
-                ['']), 'Estado 180 días post-graduación'].value_counts(
-                normalize=True)['3 Continua su búsqueda de trabajo'] * 100, 0)
-    except:
-        a_180 = 0
 
-    try:
-        a_90 = round(df.loc[-df['Estado 90 días post-graduación'].isin(['']),
-                            'Estado 90 días post-graduación'].value_counts(normalize=True)['3 Continua su búsqueda de trabajo'] * 100, 0)
-
-    except:
-        a_90=0
-
-    try:
-        actual = round(df['Estado actual'].value_counts(normalize=True)['3 Continua su búsqueda de trabajo'] * 100, 0)
-    except:
-        actual = 0
-    streamlit_metrics.metric_row({
-        '% Alumnos que Continuan buscando trabajo':
-            actual,
-
-        '% Alumnos que Continuan buscando trabajo a 90 días de graduarse':
-            a_90,
-
-        '% Alumnos que Continuan buscando trabajo a 180 días de graduarse':
-            a_180,
-
-    })
-
+# Filtros
 def opciones_filtros(df):
 
     with st.sidebar.beta_expander('Filtros:'):
-        horarios = ['']
-        horarios.extend(df['Horario'].unique().tolist())
-        horario = st.selectbox(
-            'Selecciona formato que te interesa:',
-            options=horarios)
 
-        programas = ['']
-        programas.extend(df['Programa'].unique().tolist())
-        programa = st.selectbox(
-            'Selecciona el programa que te interesa:',
-            options=programas)
-
-
-        motivacion = ['']
-        motivacion.extend(df['Motivación'].unique().tolist())
-        motivacion = st.selectbox(
-            'Selecciona la motivación del alumno:',
-            options=motivacion)
+        horario = tipo_filtro(df, 'Horario')
+        programa = tipo_filtro(df, 'Programa')
+        motivacion = tipo_filtro(df, 'Motivación')
 
         promos = df['Promoción'].unique()
         promos.sort()
@@ -150,8 +237,16 @@ def opciones_filtros(df):
                                           value=(min(promos), max(promos)))
 
 
-    return horario, programa, motivacion, promocion
+        isa = st.checkbox("Solo alumnos ISA:")
 
+    return horario, programa, motivacion, promocion, isa
+def tipo_filtro(df, tipo):
+
+    filtro_col = ['']
+    filtro_col.extend(df[tipo].unique().tolist())
+    filtro_col = st.selectbox('Selecciona el ' + tipo + ':', options=filtro_col)
+
+    return filtro_col
 def filtrar(df, filtros):
 
     if filtros[0] != '':
@@ -163,43 +258,9 @@ def filtrar(df, filtros):
     if filtros[2] != '':
         df = df.loc[df['Motivación'] == filtros[2], :]
 
+    if filtros[4]:
+        df = df[df['ISA'] != '']
 
     df = df.loc[(df['Promoción'] >= filtros[3][0]) & (df['Promoción'] <= filtros[3][1]), :]
 
     return df
-
-def estado_alumnos(df, momento, normalizar, barras):
-
-    barmode = 'relative'
-    dd2 = df.groupby(['Programa', momento])['EMAIL'].count()
-    dd2 = dd2.reset_index()
-    dd2 = dd2[(dd2.iloc[:, 1] != '') & (dd2.iloc[:, 0] != '')]
-
-    if barras:
-        barmode = 'group'
-
-
-    #No me siento orgulloso de este código
-    if normalizar:
-        total_programa = dd2.groupby('Programa')['EMAIL'].sum()
-        for i in range(len(total_programa)):
-            dd2.loc[dd2["Programa"] == total_programa.index[i], 'EMAIL'] = dd2.loc[dd2["Programa"] == total_programa.index[
-                i], 'EMAIL'] / total_programa[i]
-
-
-    fig = px.bar(x=dd2["Programa"], y=dd2["EMAIL"], color=dd2[momento],
-                  color_discrete_map={'1A Empleado a jornada completa': 'rgb(46, 204, 113)',
-                                                    '1B Prácticas a jornada completa': "rgb(22, 160, 133)",
-                                                    '1C Contrato temporal o a jornada parcial': "rgb(130, 224, 170)",
-                                                    '1D Emprende proyecto propio': 'rgb(88, 214, 141)',
-                                                    '2A Empleado en trabajo no relacionado con el bootcamp': 'rgb(241, 196, 15)',
-                                                    '2B Continuando con otros estudios': 'rgb(52, 152, 219)',
-                                                    '2C No busca trabajo por razones de salud, familiares o personales': 'rgb(95, 106, 106)',
-                                                    '3 Continua su búsqueda de trabajo': 'rgb(176, 58, 46)',
-                                                    '4 Sin información': 'rgb(33, 47, 60)',
-                                                    'Excluido de CIRR': 'rgb(27, 38, 49)'},
-                 labels=None, barmode=barmode)
-
-    st.plotly_chart(fig, use_container_width=True)
-
-
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import functions as ft
 
-csv_path = 'Outcomes'
+imagen = 'logo.png'
+sheet_name = 'Outcomes_a'
 
 ft.configuracion()
 
 good_connect = False
-
 key_file = st.sidebar.file_uploader("Clave:")
 
 if key_file is not None:
@@ -14,45 +14,27 @@ if key_file is not None:
     cliente = ft.connect_to_sheet(key_json)
     good_connect = True
 
-
 if not good_connect:
     st.warning("El archivo con los datos que has cargador no es correcto.")
     st.stop()
-
-
-sheet_name = 'Outcomes_a'
 
 worksheet = ft.open_sheet(cliente, sheet_name)
 
 
 df = ft.get_df(worksheet)
-df = df[-df['EMAIL'].isin([''])]
 
 
-menu = st.sidebar.selectbox('Menu:', options=['General', 'Estados de Contratacion'])
-
+menu = st.sidebar.selectbox('Menu:', options=['General', 'Estados de Contratacion', 'Trabajando'])
 filtros = ft.opciones_filtros(df)
 
 df = ft.filtrar(df, filtros)
 
 if menu == 'General':
-    ft.home()
-    ft.kpi_escuela(df)
-    ft.pie_tipos(df)
+    ft.menu_general(df, imagen)
 
 elif menu == 'Estados de Contratacion':
+    ft.menu_contratacion(df)
 
-    ft.kpi_busqueda_empleo(df)
+elif menu == 'Trabajando':
 
-    with st.beta_expander('Opciones:'):
-        normalizar = st.checkbox('Normalizar todos los bootcamps.')
-        barras = st.checkbox('Barras separadas')
-
-    with st.beta_expander('Estado actual', expanded=True):
-        ft.estado_alumnos(df, 'Estado actual', normalizar, barras)
-
-    with st.beta_expander('Estado 90 días post-graduación', expanded=True):
-        ft.estado_alumnos(df, 'Estado 90 días post-graduación', normalizar, barras)
-
-    with st.beta_expander('Estado 180 días post-graduación', expanded=True):
-        ft.estado_alumnos(df, 'Estado 180 días post-graduación', normalizar, barras)
+    ft.menu_trabajando(df)
