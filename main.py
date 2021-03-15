1e82ea92731b977eaa35576ff16e396fc3af8099
anderTheBridge <ander@thebridge.tech>
2021-03-15 11:00:23 +0100

    cambio de lectura

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import functions as ft
-
+import os
+import pandas as pd
 imagen = 'logo.png'
 sheet_name = 'Outcomes_a'
 
@@ -10,18 +11,21 @@ good_connect = False
 key_file = st.sidebar.file_uploader("Clave:")
 
 if key_file is not None:
-    key_json = ft.cargar_creadenciales(key_file)
-    cliente = ft.connect_to_sheet(key_json)
-    good_connect = True
 
-if not good_connect:
+    if not os.path.isfile("datos_pick"):
+        key_json = ft.cargar_creadenciales(key_file)
+        cliente = ft.connect_to_sheet(key_json)
+        worksheet = ft.open_sheet(cliente, sheet_name)
+        df = ft.get_df(worksheet)
+        df.to_pickle('datos_pick')
+
+    else:
+        df = pd.read_pickle('datos_pick')
+
+else:
     st.warning("El archivo con los datos que has cargador no es correcto.")
     st.stop()
 
-worksheet = ft.open_sheet(cliente, sheet_name)
-
-
-df = ft.get_df(worksheet)
 
 
 menu = st.sidebar.selectbox('Menu:', options=['General', 'Estados de Contratacion', 'Trabajando'])
